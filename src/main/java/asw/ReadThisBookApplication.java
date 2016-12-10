ad6e2238561210f98fb23c8fb657506b3a883d54
Antti Salmivaara <antti.salmivaara@iki.fi>
2016-12-11 01:09:53 +0200

    Creating and configuring Spring project

---
+++ src/main/java/asw/ReadThisBookApplication.java
@@ -0,0 +1,12 @@
+package asw;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReadThisBookApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ReadThisBookApplication.class, args);
+	}
+}
